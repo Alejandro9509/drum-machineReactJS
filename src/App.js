8553b2b5df34143251b0d2f58a34e996834dee50
Alejandro9509 <alejandrobringas2021@gmail.com>
2022-04-13 23:38:46 -0700

    add Description sound

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import {Container, Row, Col} from 'react-bootstrap'
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React, {useEffect} from 'react';
 
 
 const keys = [
-  ['Q', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'], 
-  ['W', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'], 
-  ['E', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'], 
-  ['A', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'], 
-  ['S', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'], 
-  ['D', 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'], 
-  ['Z', 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'], 
-  ['X', 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'], 
-  ['C', 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3']
+  ['Q', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', 'Heater1'], 
+  ['W', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', 'Heater2'], 
+  ['E', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3', 'Heater3'], 
+  ['A', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', 'Heater4'], 
+  ['S', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', 'Heater6'], 
+  ['D', 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3', 'Dsc_Oh'], 
+  ['Z', 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3', 'Kick_n_hat'], 
+  ['X', 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', 'Kick'], 
+  ['C', 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' , 'Cev']
 
 ]
 
@@ -23,13 +22,14 @@ function App () {
       window.addEventListener('keydown', e => {
         keys.map((item ) => {
           if(item[0] == e.key.toUpperCase()){
-            playFunction(item[0])
+            playFunction(item[0], item[2])
           }
         })
       });
     }, []);
-    const playFunction = (c) =>  {
+    const playFunction = (c, t) =>  {
       document.getElementById(c).play()
+      document.getElementById('display').innerText = t
     }
     return(
       <Container  id='drum-machine'>
@@ -37,7 +37,7 @@ function App () {
           <Row>
             {
               keys.slice(0, 3).map((item) => 
-                <Col onClick={() => playFunction(item[0])} key={item[0]} className='drum-pad'>
+                <Col id={item[2]} onClick={() => playFunction(item[0], item[2])} key={item[0]} className='drum-pad'>
                   {item[0]}
                   <audio  className="clip" id={item[0]} src={item[1]}></audio>
                 </Col>
@@ -47,7 +47,7 @@ function App () {
           <Row>
           {
               keys.slice(3, 6).map((item) => 
-                <Col onClick={() => playFunction(item[0])} key={item[0]} className='drum-pad'>
+                <Col id={item[2]}  onClick={() => playFunction(item[0], item[2])} key={item[0]} className='drum-pad'>
                   {item[0]}
                   <audio  className="clip" id={item[0]} src={item[1]}></audio>
                 </Col>
@@ -57,7 +57,7 @@ function App () {
           <Row>
           {
               keys.slice(6, 9).map((item) => 
-                <Col onClick={() => playFunction(item[0])} key={item[0]} className='drum-pad'>
+                <Col id={item[2]}  onClick={() => playFunction(item[0], item[2])} key={item[0]} className='drum-pad'>
                   {item[0]}
                   <audio  className="clip" id={item[0]} src={item[1]}></audio>
                 </Col>
@@ -66,7 +66,7 @@ function App () {
           </Row>
         </Container>
         <Container id="display">
-          Hola
+         
         </Container>
       </Container>)
 }
